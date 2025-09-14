97388c18dc8d7ec0daa891aabe12169957a1a845
andersonNB <andersonaugustonb@ufps.edu.co>
2025-09-14 11:45:54 -0500

    feat: take and skip in todos endpoint

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,7 +2,24 @@ import prisma from "@/lib/prisma";
 import {NextResponse} from "next/server";
 
 export async function GET(request: Request) {
-	const todos = await prisma.todo.findMany();
+	const {searchParams} = new URL(request.url);
+	console.log(searchParams);
+	const take = searchParams.get("take") ?? "10";
+	const skip = searchParams.get("skip") ?? "0";
+
+	if (isNaN(+take) || isNaN(Number(skip))) {
+		return NextResponse.json(
+			{message: "Take tiene que ser un número"},
+			{
+				status: 400,
+			}
+		);
+	}
+
+	const todos = await prisma.todo.findMany({
+		take: Number(take),
+		skip: Number(skip),
+	});
 
 	return NextResponse.json(todos);
 }
