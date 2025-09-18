cfac59d9b34b6486ddc20eed74e5ac3625b9de85
andersonNB <andersonaugustonb@ufps.edu.co>
2025-09-17 21:09:26 -0500

    feat: refactoring layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
+import WidgetItem from "@/components/WidgetItem/WidgetItem";
 import React from "react";
 
 const DashboardPages = () => {
-	return <div>Dashboardpages</div>;
+	return (
+		<div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+			{/* TODO: src/components <WidgetItem /> */}
+			<WidgetItem />
+			{/* TODO: Fin <WidgetItem /> */}
+		</div>
+	);
 };
 
 export default DashboardPages;
