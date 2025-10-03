c78a0fc3a1c69755c26445ab713d7f8356f4a043
andersonNB <andersonaugustonb@ufps.edu.co>
2025-10-03 12:20:37 -0500

    feat: providers google

--- next.config.ts
+++ next.config.ts
@@ -21,8 +21,12 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-
 				hostname: "avatars.githubusercontent.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
 				pathname: "/**",
 			},
 		],
