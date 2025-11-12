10e3dfa7abe4a1be243f188703223d5257ab94c1
AndersonnbNE <a.navarro@nelumbo.com.co>
2025-11-11 20:41:30 -0500

    feat: waching a little behavior of react

--- src/app/dashboard/react/page.tsx
+++ src/app/dashboard/react/page.tsx
@@ -1,23 +1,49 @@
 "use client";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
-function Child({name}: {name: string}) {
+function Child({
+	name = "Se activa por que aunque no se le pasan props, al estar en el componente padre el render del padre activa el render del hijo",
+}: {
+	name?: string;
+}) {
 	console.log("Render hijo");
-	return <p>Hola {name}</p>;
+
+	const [highlight, setHighlight] = useState(false);
+
+	useEffect(() => {
+		// Activa el highlight brevemente cada vez que el componente se renderiza
+		setHighlight(true);
+		const timeout = setTimeout(() => setHighlight(false), 2000);
+		return () => clearTimeout(timeout);
+	}, [setHighlight]);
+
+	return (
+		<p
+			className={`border-2 w-[500px] h-[80px] transition-all duration-500 p-2 ${
+				highlight
+					? "border-amber-400 shadow-md scale-105"
+					: "border-transparent shadow-none scale-100"
+			}`}
+		>
+			{name}
+		</p>
+	);
 }
 
 const Parent = () => {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div>
-			<button
-				onClick={() => setCount(count + 1)}
-				className="bg-blue-500 text-white p-2 rounded-md"
-			>
-				Incrementar
-			</button>
-			<Child name="Anderson" />
+		<div className="flex flex-col gap-4 ">
+			<div>
+				<button
+					onClick={() => setCount(count + 1)}
+					className="bg-blue-500 text-white p-2 rounded-md hover:cursor-pointer"
+				>
+					Incrementar
+				</button>
+			</div>
+			<Child key={count} />
 		</div>
 	);
 };
